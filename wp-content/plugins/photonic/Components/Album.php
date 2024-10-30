236cb9c6187a04774cc650ad48b67df2d31c5610
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-10-30 22:12:50 +0100

    cchi sono

---
+++ wp-content/plugins/photonic/Components/Album.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Photonic_Plugin\Components;
+
+class Album {
+	public $id;
+
+	public $thumbnail;
+	public $tile_image;
+
+	public $thumb_size;
+	public $tile_size;
+	public $thumbnail_id;
+
+	public $main_page;
+	public $gallery_url;
+
+	public $title;
+	public $description;
+	public $counter;
+
+	public $classes = [];
+	public $passworded;
+	public $realm_id;
+	public $data_attributes = [];
+}
